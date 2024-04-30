481c207b0bcb583fb4db472f886b03259be6ee33
heyjoeway <joe@jojudge.com>
2024-04-30 16:45:15 -0400

    Trying to get things working on mobile

--- src/Animations.ts
+++ src/Animations.ts
@@ -9,6 +9,7 @@ function blurFall(node: HTMLElement, params?: {
 		css: (t: number) => {
 			const radius = params?.radius || 64;
 			t = quartOut(t);
+			return "";
 			return `
 				filter: blur(${radius * (1 - t)}px) opacity(${100 * t}%);
 				transform: scale(${1 + (1 - t)}) rotateZ(${15 * (1 - t)}deg);
@@ -25,6 +26,7 @@ function blurSink(node: HTMLElement, params?: {
 		duration: params?.duration || 500,
 		css: (t: number) => {
 			const radius = params?.radius || 64;
+			return "";
 			return `
 				filter: blur(${radius * (1 - t)}px) opacity(${100 * t}%);
 				transform: scale(${t});
